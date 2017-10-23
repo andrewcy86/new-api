e87ba9dd8348f58df0c1d9e1f8e4ceb346e6d7ab
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-10-23 11:54:14 -0400

    Delete db_login.php

--- db_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Edit this information with the datbase login information
-$db_host = $service['credentials']['host'];
-$db_username = $service['credentials']['username'];
-$db_password = $service['credentials']['password'];
-$db_name = $service['credentials']['db_name'];
-?>
