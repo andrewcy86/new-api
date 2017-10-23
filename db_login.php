3b73a15ce0592482645f1509a8334cd06bc19720
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-10-23 11:34:08 -0400

    Add files via upload

---
+++ db_login.php
@@ -0,0 +1,7 @@
+<?php
+// Edit this information with the datbase login information
+$db_host = $service['credentials']['host'];
+$db_username = $service['credentials']['username'];
+$db_password = $service['credentials']['password'];
+$db_name = $service['credentials']['db_name'];
+?>
