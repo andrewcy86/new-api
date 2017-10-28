716e564d13f64f1a9ba9de8228b1dd822c4f335f
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-10-27 22:49:54 -0400

    Update view-table-data.php

--- pages/view-table-data.php
+++ pages/view-table-data.php
@@ -230,9 +230,9 @@ $api_operations = sanitize_text_field($_POST['api_operation']);
 				$handle = fopen($_FILES['csv_data']['tmp_name'], "r");
 				while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 					
-$get_column_count = ("select count(*) AS colTotal FROM information_schema.columns WHERE table_name = $api_table_name");
+//$get_column_count = ("select count(*) AS colTotal FROM information_schema.columns WHERE table_name = $api_table_name");
 // Show column count
-$column_count_result = mysqli_query($conn, $get_column_count);
+//$column_count_result = mysqli_query($conn, $get_column_count);
 //$col_count = $column_count_result->fetch_object()->colTotal;
 
 $get_column_name = "SHOW COLUMNS FROM $api_table_name";
@@ -260,7 +260,7 @@ $column_name_final = implode(', ', array_slice($columns, 1));
 $item1 = mysqli_real_escape_string($conn,$data[0]);
 $item2 = mysqli_real_escape_string($conn,$data[1]);
 					
-$import = "INSERT INTO $api_table_name($column_name_final) VALUES ('$item1','$item2')";
+$import = "INSERT INTO $api_table_name('name','email') VALUES ('$item1','$item2')";
 					
 
 					mysqli_query($conn,$import);
