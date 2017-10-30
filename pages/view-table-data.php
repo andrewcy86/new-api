5684a69a2a15d322f08edd6c8e161e0ec8d34350
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-10-30 10:46:52 -0400

    Update view-table-data.php

--- pages/view-table-data.php
+++ pages/view-table-data.php
@@ -249,7 +249,7 @@ $s = 0;
 for ($k = 0 ; $k <= $col_count; $k++){ 
 $i++; 
 $s++;
-    $item[$s] = mysqli_real_escape_string($conn,$data['.$i.']);
+    $item[$s] = mysqli_real_escape_string($conn,$data[$i]);
 }
 					
 
