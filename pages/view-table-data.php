10b30a712fafa577b73fd987763af34154374fab
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-10-30 08:24:11 -0400

    Update view-table-data.php

--- pages/view-table-data.php
+++ pages/view-table-data.php
@@ -226,15 +226,6 @@ $api_operations = sanitize_text_field($_POST['api_operation']);
 				
 			$arrFileName = explode('.',$_FILES['csv_data']['name']);
 
-			if($arrFileName[1] == 'csv'){
-				$handle = fopen($_FILES['csv_data']['tmp_name'], "r");
-				while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-					
-//$get_column_count = ("select count(*) AS colTotal FROM information_schema.columns WHERE table_name = $api_table_name");
-// Show column count
-//$column_count_result = mysqli_query($conn, $get_column_count);
-//$col_count = $column_count_result->fetch_object()->colTotal;
-
 $get_column_name = "SHOW COLUMNS FROM $api_table_name";
 $col_name = mysqli_query($conn, $get_column_name);
 
@@ -243,8 +234,15 @@ while($row = $col_name->fetch_assoc()){
 }
 
 $column_name_final = implode(',', array_slice($columns, 1));
-
-
+				
+			if($arrFileName[1] == 'csv'){
+				$handle = fopen($_FILES['csv_data']['tmp_name'], "r");
+				while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+					
+//$get_column_count = ("select count(*) AS colTotal FROM information_schema.columns WHERE table_name = $api_table_name");
+// Show column count
+//$column_count_result = mysqli_query($conn, $get_column_count);
+//$col_count = $column_count_result->fetch_object()->colTotal;
 
 //echo $col_count;
 //$i = -1;
@@ -260,7 +258,7 @@ $column_name_final = implode(',', array_slice($columns, 1));
 $item1 = mysqli_real_escape_string($conn,$data[0]);
 $item2 = mysqli_real_escape_string($conn,$data[1]);
 					
-$import = "INSERT INTO $api_table_name($column_name_final) VALUES ('$item1','$item2')";
+$import = "INSERT INTO $api_table_name($column_name_final) VALUES ('$item1','$column_name_final')";
 					
 
 					mysqli_query($conn,$import);
