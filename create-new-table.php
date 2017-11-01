d4eb3b18884f216168612400da31053dc4d296d1
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-01 10:43:09 -0400

    Update create-new-table.php

--- create-new-table.php
+++ create-new-table.php
@@ -21,7 +21,8 @@ if (!$conn) {
 	$safe_api_description = sanitize_text_field($_POST['api_description']);
 	$safe_api_version = sanitize_text_field($_POST['api_version']);
 	$safe_api_operations = sanitize_text_field($_POST['api_operations']);
-			
+	$safe_api_users = sanitize_text_field($_POST['api_users']);
+	
 	/** Row Name Text Input */
 	$row_names = $_POST['name'];
 	$row_types = $_POST['type'];
@@ -82,8 +83,8 @@ if (!$conn) {
 
 
 
-$insert_api_q = "INSERT INTO api_data (api_name, api_description, api_version, api_table, api_operations)
-VALUES ('$safe_table_name', '$safe_api_description', '$safe_api_version', '$table_name', '$safe_api_operations')";
+$insert_api_q = "INSERT INTO api_data (api_name, api_description, api_version, api_table, api_operations, api_users)
+VALUES ('$safe_table_name', '$safe_api_description', '$safe_api_version', '$table_name', '$safe_api_operations', '$safe_api_users')";
 
 $check_api_q = "SELECT 'id' FROM 'api_data' WHERE 'api_table' = '$table_name'";
 
@@ -94,62 +95,7 @@ $error_url_redirect = admin_url( 'admin.php?page=create-db-tables&create_new_tab
 wp_redirect( $error_url_redirect );
 } else {
  mysqli_query($conn, $insert_api_q);	
-}
-
-
-/**
-//EPA Create the coressponding PHP CRUD index.php file and folder
-
-	$dirname = $_SERVER['DOCUMENT_ROOT']."/api/".$safe_table_name;  
-    $filename = $_SERVER['DOCUMENT_ROOT']."/api/".$safe_table_name."/index.php";  
-	  
-    if (!file_exists($dirname)) {  
-        //echo "The directory {$dirname} exists";  
-    //} else {  
-        mkdir($dirname, 0755);  
-//$content = file_get_contents($_SERVER['DOCUMENT_ROOT']."/api/"."api_example.php");
-	
-	
-$file = 'api_example.php';  
-$appendBefore = '<?php 
-require_once($_SERVER["DOCUMENT_ROOT"]."/api/"."db_login.php");
-header("Access-Control-Allow-Origin: *");
-$con = mysqli_connect($db_host,$db_username,$db_password,$db_name);
-
-//MOD ID
-
-$base_dir = basename(__DIR__);
-
-$query = "SELECT id, api_name, api_description, api_version, api_table, api_operations FROM api_data WHERE api_name = \'$base_dir\'";
-
-$result = $con->query($query);
-
-if ($result->num_rows > 0) {
-  
-    while($row = $result->fetch_assoc()) {
-$api_name = $row["api_name"];
-$api_description = $row["api_description"];
-$api_version = $row["api_version"];
-$api_table = $row["api_table"];
-$api_operations = $row["api_operations"];
-     }
-
-}
-$con->close();
-
-$ops_array = explode(", ", $api_operations);';
- 
-$temp = file_get_contents($_SERVER['DOCUMENT_ROOT']."/api/".$file);
-$content = $appendBefore.$temp;
-file_put_contents($file, $content);
-
-
-	$fp = fopen($filename,"wb"); 
-	fwrite($fp,$content); 
-	fclose($fp); 
-       // echo "The directory {$dirname} was successfully created.";  
-    }  
-*/	
+}	
 	
 	$succuss_url_redirect = admin_url( "admin.php?page=create-db-tables&create_new_table_success=true&table_name=$table_name" );
     wp_redirect( $succuss_url_redirect );
