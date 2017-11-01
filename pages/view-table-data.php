d4a40ca82a29c664e09e796b572c9c9887c3eb19
Andrew Yuen <andrewcy86@users.noreply.github.com>
2017-11-01 16:43:04 -0400

    Update view-table-data.php

--- pages/view-table-data.php
+++ pages/view-table-data.php
@@ -238,8 +238,8 @@ $api_name = sanitize_text_field($_POST['data']);
 $api_description = sanitize_text_field($_POST['api_description']);
 $api_version = sanitize_text_field($_POST['api_version']);
 $api_operations = sanitize_text_field($_POST['api_operation']);
-$api_users= sanitize_text_field($_POST['api_users']);
-	
+$api_users= sanitize_text_field(implode(",",$_POST['api_users']));
+
 			if($_FILES['csv_data']['name']){
 			$api_table_name = 'wp_api_'.$api_name;
 // Begin Update Process by Truncating Table
